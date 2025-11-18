c39b40b31f60a07a0bfae3c4f2992e262416f6c7
Milkah Mwangi <gathonimilkahh@gmail.com>
2025-11-18 18:57:51 +0300

    removed comments

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-/*Function 1: calculateTax
-Create a function named calculateTax that takes a single parameter, amount, representing a monetary value.
-The function should calculate a 10% tax on the amount and return the tax value. */
 
 function calculateTax(amount) {
   return (amount * 0.10)
@@ -9,20 +6,11 @@ function calculateTax(amount) {
 let taxValue = calculateTax(89)
 console.log(taxValue)
 
-
-/* Function 2: convertToUpperCase
-Create a function named convertToUpperCase that takes a single parameter, text, which is a string.
-The function should convert the string to uppercase and return the result. */
-
-
 function convertToUpperCase(text) {
   return text.toUpperCase()
 }
 console.log(convertToUpperCase("hi"))
 
-/* Function 3: findMaximum
-Create a function named findMaximum that takes two parameters, num1 and num2, which are numbers.
-The function should return the larger of the two numbers.*/
 
 function findMaximum(num1, num2) {
   if (num1 > num2) { return num1 }
@@ -32,19 +20,11 @@ function findMaximum(num1, num2) {
 let max = findMaximum(9, 11)
 console.log(max)
 
-/* Function 4: isPalindrome
-Create a function named isPalindrome that takes a single parameter, word, which is a string.
-The function should return true if the string is a palindrome (reads the same forward and backward), and false otherwise. */
-
 function isPalindrome(word) {
   let reversedWord = word.split("").reverse().join("")
   return word === reversedWord
 }
 console.log(isPalindrome("mum"))
-
-/*Function 5: calculateDiscountedPrice
-Create a function named calculateDiscountedPrice that takes two parameters: originalPrice and discountPercentage.
-The function should calculate and return the price after applying the discount. For example, if originalPrice is $100 and discountPercentage is 20, the function should return */
 
 function calculateDiscountedPrice(originalPrice, discountPercentage) {
   return (originalPrice) - (originalPrice * (discountPercentage / 100))
